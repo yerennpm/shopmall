b8d9a21e6fbc57d4048c6966444ffbae6ebb8381
wangxi <979547277@qq.com>
2021-10-04 22:35:06 +0800

    '推送'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,11 +3,16 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
-    <home-recommend :recommends="recommends"></home-recommend>
-    <home-feature></home-feature>
-    <tab-control :titles="['流行', '新款', '精选']" class="tab-control"></tab-control>
-    <goods-list :goods="goods['pop'].list"></goods-list>
+    <scroll class="content" ref="scroll">
+      <home-swiper :banners="banners"></home-swiper>
+      <home-recommend :recommends="recommends"></home-recommend>
+      <home-feature></home-feature>
+      <tab-control :titles="['流行', '新款', '精选']"
+                   class="tab-control"
+                   @tabClick="tabClick"></tab-control>
+      <goods-list :goods="goods[currentType].list"></goods-list>
+    </scroll>
+    <back-top @click.native="backClick"></back-top>
   </div>
 </template>
 
@@ -16,6 +21,8 @@
   import NavBar from "../../components/common/navbar/NavBar";
   import TabControl from "../../components/content/TabControl/TabControl";
   import GoodsList from "../../components/content/goods/GoodsList";
+  import Scroll from "../../components/common/scroll/Scroll";
+  import BackTop from "../../components/content/backTop/BackTop";
 
   import HomeSwiper from "./childComps/HomeSwiper";
   import HomeRecommend from "./childComps/HomeRecommend";
@@ -23,6 +30,8 @@
 
   import {getHomeMultidata} from "../../network/home";
   import {getHomeGoods} from "../../network/home";
+
+
 
   export default {
     name: "Home",
@@ -44,7 +53,9 @@
       GoodsList,
       HomeSwiper,
       HomeRecommend,
-      HomeFeature
+      HomeFeature,
+      Scroll,
+      BackTop
     },
     created() {
       this.getHomeMultidata()
@@ -54,6 +65,27 @@
       this.getHomeGoods('sell')
     },
     methods: {
+      //事件监听方法
+      tabClick(index) {
+        switch(index) {
+          case 0:
+            this.currentType = 'pop'
+            break
+          case 1:
+            this.currentType = 'new'
+            break
+          case 2:
+            this.currentType = 'sell'
+        }
+
+      },
+      backClick() {
+        this.$refs.scroll.scrollTo(0, 0)
+      },
+
+
+
+      //网络请求方法
       getHomeMultidata() {
         getHomeMultidata().then(res => {
           this.banners = res.data.banner.list
@@ -75,7 +107,7 @@
 
 <style scoped>
   #home {
-  padding-top: 44px;
+    padding-top: 44px;
   }
   .home-nav {
     background-color: var(--color-high-text);
@@ -89,5 +121,14 @@
   .tab-control {
     position: sticky;
     top: 44px;
+  }
+  .content {
+    overflow: hidden;
+
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
   }
 </style>
